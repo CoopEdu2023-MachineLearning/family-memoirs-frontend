cfc5cc8b659bd3650f2caf322d6475c8940127d7
vio3500 <vio3500@github>
2025-05-15 14:47:59 +0800

    Refactor TitleComponents to enhance date and location formatting; update styles for improved layout

--- src/components/ArticleComponents/TitleComponents/index.jsx
+++ src/components/ArticleComponents/TitleComponents/index.jsx
@@ -1,64 +1,85 @@
 import React from 'react';
-    import PropTypes from 'prop-types';
-    import styles from './index.module.scss';
+import PropTypes from 'prop-types';
+import styles from './index.module.scss';
 
-    function TitleComponents({title, era, location, startYear, endYear, startMonth, endMonth}) {
-        const formatDate = () => {
-            // 只有起始年代
-            if (era && !startYear && !endYear && !startMonth && !endMonth) {
-                return `${String(era).substring(0, 4)}年代`;
-            }
+function TitleComponents({era, location, startYear, endYear, startMonth, endMonth}) {
+    const formatDateAndLocation = () => {
+        // 只有起始年代
+        if (era && !startYear && !endYear && !startMonth && !endMonth) {
+            return `${String(era).substring(2, 4)}年代 ${location}`;
+        }
 
-            // 只有起始年
-            if (startYear && !endYear && !startMonth && !endMonth) {
-                return `${startYear}年`;
-            }
+        // 只有起始年
+        if (startYear && !endYear && !startMonth && !endMonth) {
+            return `${startYear}年 ${location}`;
+        }
 
-            // 起始年和结束年
-            if (startYear && endYear && !startMonth && !endMonth) {
-                return `${startYear}年 - ${endYear}年`;
-            }
-
-            // 只有起始年和月
-            if (startYear && startMonth && !endYear && !endMonth) {
-                return `${startYear}年${startMonth}月`;
-            }
-
-            // 完整的起始和结束时间
-            if (startYear && startMonth && endYear && endMonth) {
-                return `${startYear}年${startMonth}月 - ${endYear}年${endMonth}月`;
-            }
-
-            return '时间未知';
-        };
-
-        return (
-            <div className={styles.titleContainer}>
-                <h1 className={styles.title}>{title}</h1>
-                <div className={styles.metaInfo}>
-                    <div className={styles.metaItem}>
-                        <label>时间</label>
-                        <div>{formatDate()}</div>
+        // 起始年和结束年
+        if (startYear && endYear && !startMonth && !endMonth) {
+            return (
+                <>
+                    <div className={styles.date_style1}>
+                        <span>{startYear}年-{endYear}年</span>
+                        <span>{location}</span>
                     </div>
-                    {location && (
-                        <div className={styles.metaItem}>
-                            <label>地点</label>
-                            <div>{location}</div>
-                        </div>
-                    )}
-                </div>
-            </div>
-        );
-    }
+                </>
+            );
+        }
 
-    TitleComponents.propTypes = {
-        title: PropTypes.string.isRequired,
-        era: PropTypes.number,
-        location: PropTypes.string,
-        startYear: PropTypes.number,
-        endYear: PropTypes.number,
-        startMonth: PropTypes.number,
-        endMonth: PropTypes.number
+        // 只有起始年和月
+        if (startYear && startMonth && !endYear && !endMonth) {
+            return (
+                <div className={styles.date_style1}>
+                    <span>{startYear}年{startMonth}月</span>
+                    <span>{location}</span>
+                </div>
+            );
+        }
+
+        // 完整的起始和结束时间
+        if (startYear && startMonth && endYear && endMonth) {
+            if (startYear === endYear) {
+                return (
+                    <div className={styles.date_style2}>
+                        <span>{startYear}年{startMonth}月-{endMonth}月</span>
+                        <span>{location}</span>
+                    </div>
+                )
+            } else {
+                return (
+                    <div className={styles.date_style2}>
+                        <span>{startYear}年{startMonth}月</span>
+                        <div className={styles.second_line}>至
+                            <div>{endYear}年{endMonth}月</div>
+                        </div>
+                        <span>{location}</span>
+                    </div>
+                );
+            }
+        }
+
+        return '时间未知';
     };
 
-    export default TitleComponents;
+    return (
+        <div className={styles.titleContainer}>
+            <div className={styles.metaInfo}>
+                <div className={styles.metaItem}>
+                    <div>{formatDateAndLocation()}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+TitleComponents.propTypes = {
+    title: PropTypes.string.isRequired,
+    era: PropTypes.number,
+    location: PropTypes.string,
+    startYear: PropTypes.number,
+    endYear: PropTypes.number,
+    startMonth: PropTypes.number,
+    endMonth: PropTypes.number
+};
+
+export default TitleComponents;
