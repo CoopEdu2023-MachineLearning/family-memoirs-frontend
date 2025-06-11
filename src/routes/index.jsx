dd36fa31ee85d0f2873202422f81e43ad7dfa566
winter7zowo <winter7z@163.com>
2025-06-12 02:05:45 +0800

    merge branch 'ray-zeng' into dev.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -34,7 +34,6 @@ const routes = [
   {
     element: <PublicProtectedRoute />,
     children: [
-<<<<<<< HEAD
       { path: '/login', element: <LoginPage name='login' /> }
     ]
   },
@@ -46,14 +45,6 @@ const routes = [
     path: '/demo',
     element: <LoginPage name='demo' />
   },
-=======
-      { path: '/login', element: <DemoPage name="login" /> }
-    ]
-  },
-  { path: '/home', element: <DemoPage name="home" /> },
-  { path: '/demo', element: <DemoPage name="demo" /> },
-  { path: '/article/:id', element: <ArticlePage /> }
->>>>>>> 6269ec13b8c37fc908565863160788de42a213ff
 ];
 
 export default routes;
