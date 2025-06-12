0401ba83519b21de7c888f82f2456ecd54593e66
winter7zowo <winter7z@163.com>
2025-06-12 13:19:02 +0800

    change format of searchApi

--- src/apis/searchAPI/index.jsx
+++ src/apis/searchAPI/index.jsx
@@ -1,21 +1,18 @@
 import http from "@http";
 import { replaceError } from "../../utils";
 
-const searchApi = {
-    search: async (q, snippetLength, preciseSearch) => {
-        const result = await http.get("/search", {
-            params: {
-                q,
-                snippetLength,
-                preciseSearch,
-            }
-        }).catch(replaceError("Failed to search stories"));
-        return {
-            stories: result.stories.hits,
-            tellers: result.tellers.hits,
-            users: result.users.hits,
-        };
-    }
+const searchApi = (q, snippetLength, preciseSearch) => {
+    return http.get("/search", {
+        params: {
+            q,
+            snippetLength,
+            preciseSearch,
+        }
+    }).then(result => ({
+        stories: result.stories.hits,
+        tellers: result.tellers.hits,
+        users: result.users.hits,
+    })).catch(replaceError("Failed to search stories"));
 };
 
 export default searchApi;
